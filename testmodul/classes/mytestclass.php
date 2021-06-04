12f54fc60c22e715ea332e90c6ccef72772bb5de
stuck1a <60481324+stuck1a@users.noreply.github.com>
2021-06-04 07:08:54 +0200

    Add files via upload

---
+++ testmodul/classes/mytestclass.php
@@ -0,0 +1,8 @@
+<?php
+namespace own\owntestmodul\classes;
+
+class ownmytestclass {   }
+
+echo "Test";
+
+?>
---
+++ testmodul/metadata.php
@@ -0,0 +1,40 @@
+<?php
+
+$sMetadataVersion = '2.1';
+
+$aModule = [
+    'id'           => 'owntestmodul',
+    'title'        => 'Testmodul',
+    
+    'extend'       => [  
+      // Model
+      \OxidEsales\Eshop\Application\Model\Article::class => own\owntestmodul\Application\Model\owntestmodul__oxarticle::class
+    ],
+    
+    'controllers'  => [   
+    ],
+    
+    'blocks'       => [
+    ],
+    
+    'settings'     => [
+      [
+        'group' => 'owntestmodul_general',
+        'name' => 'sTestsetting1',
+        'type' => 'str',
+        'value' => 'Testvalue1'
+      ],
+      
+      [
+        'group' => 'owntestmodul_general',
+        'name' => 'sTestsetting2',
+        'type' => 'select',
+        'value' => 'download',
+        'constraints' => 'download|inline'
+      ]
+    ]
+    
+    'templates'    => [
+    // tpl files
+    ]
+];
