a285bea62e2cedc14c258c396f0ef16b3daa61db
stuck1a <60481324+stuck1a@users.noreply.github.com>
2021-09-12 16:02:41 +0200

    Add files via upload

---
+++ s1atestmodul/Application/models/s1atestmodul__oxarticle.php
@@ -0,0 +1,12 @@
+<?php
+namespace s1a\s1atestmodul\Application\Model;
+
+class s1atestmodul__oxarticle extends s1atestmodul__oxarticle_parent {
+  
+  public function testfunktion() {
+    return "Testfunktion aufgerufen!";   
+  }
+    
+}  
+
+?>
---
+++ s1atestmodul/classes/s1a_Testklasse.php
@@ -0,0 +1,8 @@
+<?php
+namespace s1a\s1atestmodul\classes;
+
+class s1a_Testklasse {   }
+
+echo "Testklasse Controller wurde durchlaufen!";  // debug line
+
+?>
